f916aa086719c6188ff07af97be24d57dce85518
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2023-06-22 22:24:33 +0200

    [#3] - Structure navbar + fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import Nav from "@/components/nav";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/nav.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Nav() {
+  return (
+    <nav className="text-center bg-orange-200 p-6 font-semibold text-4xl text-black">
+      Atelier de Delphine
+    </nav>
+  );
+}
