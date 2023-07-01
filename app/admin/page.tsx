0d195b25ce5f8b16f02f2b6d20db280e40b8318a
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2023-07-01 19:49:46 +0200

    [#8] - Base Layout admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,10 +5,14 @@ export default async function Admin() {
   const session = await loginIsRequiredServer();
 
   return (
-    <div>
-      Admin Panel:
-      {session?.user?.email}
-      <LogoutButton />
+    <div className="m-auto max-w-6xl text-black">
+      <div className="m-1 p-2">
+        <span className="font-semibold">Page d&apos;administration</span>
+        <span className="float-right">
+          <LogoutButton />
+        </span>
+        {/* Liste des posts */}
+      </div>
     </div>
   );
 }
--- components/logoutBtn.tsx
+++ components/logoutBtn.tsx
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 export const LogoutButton = () => {
   return (
     <button
-      className="py-1 px-2 rounded-md bg-red-400 hover:bg-red-600 m-2"
+      className="py-1 px-2 rounded-md text-white bg-red-400 hover:bg-red-600 m-2"
       onClick={() => signOut()}
     >
       Déconnexion
